e86267307f20fdfcbcdd0083c27328dc9b3c1419
Johnny Chan <JohnnyChan@yuanta.com>
2020-10-30 17:04:06 +0800

    Add include file

---
+++ include.h
@@ -0,0 +1,28 @@
+/* IO, string, and assert */
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+#include <iomanip>
+#include <string>
+using std::string;
+#include <cassert>
+
+/* STL containers */
+#include <vector>
+using std::vector;
+#include <unordered_map>
+using std::unordered_map;
+#include <deque>
+using std::deque;
+#include <queue>
+using std::queue;
+using std::priority_queue;
+#include <map>
+using std::map;
+
+/* limits such as INT_MIN, INT_MAX, etc */
+#include <climits>
+
+/* types such as isdigit, isalphanumeric, etc */
+#include <cctype>
