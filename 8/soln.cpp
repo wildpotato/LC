f649af403ebc72174623de77c5f8d5e53b203e9a
Johnny Chan <chanyao@umich.edu>
2020-11-02 22:43:03 +0800

    Rewrite algo completely

--- 8/soln.cpp
+++ 8/soln.cpp
@@ -3,79 +3,38 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        int beg = 0, len = 0;
-        long ret;
-        bool detect_num_start = false;
-        int detect_neg_sign = -1;
-        int detect_pos_sign = -1;
-        for (int i = 0; i < s.length(); ++i) {
-            if (isdigit(s[i])) {
-                if (!detect_num_start) {
-                    if (detect_neg_sign >= 0 && detect_pos_sign >= 0) {
-                        return 0;
-                    }
-                    detect_num_start = true;
-                    if (detect_neg_sign >= 0 && detect_neg_sign == i-1) {
-                        beg = i-1;
-                        len = 2;
-                    } else if (detect_pos_sign >= 0 && detect_pos_sign == i-1) {
-                        beg = i-1;
-                        len = 2;
-                    } else {
-                        detect_neg_sign = -1;
-                        detect_pos_sign = -1;
-                        beg = i;
-                        len++;
-                    }
-                } else {
-                    len++;
-                }
-            } else {
-                if (detect_num_start) {
-                    break; // get substr for num already
-                }
-                else if (isspace(s[i]) && !detect_num_start) {
-                    continue;
-                } else if (!detect_num_start && s[i] == '-') {
-                    detect_neg_sign = i;
-                } else if (!detect_num_start && s[i] == '+') {
-                   detect_pos_sign = i;
-                } else {
-                    return 0;
-                }
-            }
-        }
-
-        string num = s.substr(beg, len);
-        if (num.length() == 0) {
-            return 0;
-        }
-        // trim leading 0's
-		int trim_start = (num[0] == '+' || num[0] == '-') ? 1 : 0;
-		int trim_end = num.find_first_not_of('0', trim_start);
-		num = num.erase(trim_start, trim_end - trim_start);
-        len = num.length();
-        cout << "|" << num << "|" << endl;
-        cout << "len=" << len << endl;
-        if (len == 0) {
-            return 0;
+		int i = 0, sign = 0;
+		long long res = 0;
+		while (isspace(s[i])) {
+			++i;
 		}
-        if (len >= 11) { // out of range for sure
-          if (num[0] == '-') {
-              return INT_MIN;
-          }
-          return INT_MAX;
-        }
-        ret = stol(num);
-        cout << "ret = '" << ret << "'" << endl;
-        if (ret > INT_MAX) {
-            return INT_MAX;
-        } else if (ret < INT_MIN) {
-            return INT_MIN;
-        } else {
-            return ret;
-        }
-    }
+		if (s[i] == '+') {
+			sign = 1;
+			++i;
+		} else if (s[i] == '-') {
+			sign = -1;
+			++i;
+		}
+		for (; i < s.length(); ++i) {
+			if (isdigit(s[i])) {
+				res = res * 10 + s[i]-'0';
+				cout << "|i = " << i << " RES=" << res << "|";
+			} else {
+				break;
+			}
+		}
+		if (res > INT_MAX) {
+			if (sign < 0) {
+				res = INT_MIN;
+			} else {
+				res = INT_MAX;
+			}
+		} else {
+			res = (sign < 0) ? -1 * res : res;
+		}
+		cout << "res = " << res << endl;
+		return res;
+	}
 };
 
 
