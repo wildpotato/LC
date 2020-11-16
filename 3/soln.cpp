1d8c080367b215ef6711c93e867f9b05e2c03b2e
Johnny Chan <JohnnyChan@yuanta.com>
2020-11-16 16:51:04 +0800

    Add default cases

--- 3/soln.cpp
+++ 3/soln.cpp
@@ -7,6 +7,34 @@ public:
     }
 };
 
+void test1() {
+    string s = "abcabcbb";
+    Solution soln;
+    assert(soln.lengthOfLongestSubstring(s) == 3);
+}
+
+void test2() {
+    string s = "bbbbb";
+    Solution soln;
+    assert(soln.lengthOfLongestSubstring(s) == 1);
+}
+
+void test3 {
+    string s = "pwwkew";
+    Solution soln;
+    assert(soln.lengthOfLongestSubstring(s) == 3);
+}
+
+void test4 {
+    string s = "";
+    Solution soln;
+    assert(soln.lengthOfLongestSubstring(s) == 0);
+}
+
 int main() {
+    test1;
+    test2;
+    test3;
+    test4;
     return 0;
 }
