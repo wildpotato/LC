037f8f3718fcfb398872e54cb7181458ad8c5f2b
Johnny Chan <chanyao@umich.edu>
2020-11-16 23:00:43 +0800

    Compiles but buggy

--- 3/soln.cpp
+++ 3/soln.cpp
@@ -3,8 +3,25 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
+		int bestSoFar = 0;
+		for (unsigned int i = 0; i < s.length()-1; ++i) {
+			int soFar = 0;
+			for (unsigned int j = i + 1; j < s.length(); ++j) {
+				if (s.substr(i, j-i).find(s[j]) != string::npos) {
+					break;
+				}
+				else {
+					soFar++;
+					if (soFar > bestSoFar) {
+						bestSoFar = soFar;
+					}
+				}
+			}
+		}
+		cout << "bestSoFar=" << bestSoFar << "\n";
+		return bestSoFar;
     }
+
 };
 
 void test1() {
@@ -19,22 +36,22 @@ void test2() {
     assert(soln.lengthOfLongestSubstring(s) == 1);
 }
 
-void test3 {
+void test3() {
     string s = "pwwkew";
     Solution soln;
     assert(soln.lengthOfLongestSubstring(s) == 3);
 }
 
-void test4 {
+void test4() {
     string s = "";
     Solution soln;
     assert(soln.lengthOfLongestSubstring(s) == 0);
 }
 
 int main() {
-    test1;
-    test2;
-    test3;
-    test4;
+    test1();
+    test2();
+    test3();
+    test4();
     return 0;
 }
