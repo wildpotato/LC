b15021a151ffc1f6a707b0046ceb94abda0a6eed
Johnny Chan <JohnnyChan@yuanta.com>
2020-11-18 07:52:18 +0800

    Accepted and beat 100%

--- 1342/soln.cpp
+++ 1342/soln.cpp
@@ -3,7 +3,19 @@
 class Solution {
 public:
     int numberOfSteps (int num) {
-
+        int count = 0;
+        if (num == 0) {
+            return count;
+        }
+        while (num != 0) {
+            if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                num -= 1;
+            }
+            count++;
+        }
+        return count;
     }
 };
 
