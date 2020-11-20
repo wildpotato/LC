141a771edf81f511718f0a4a65af199bb0d66d81
Johnny Chan <chanyao@umich.edu>
2020-11-21 07:14:25 +0800

    Accepted and beat 100% with less memory used

--- 70/soln.cpp
+++ 70/soln.cpp
@@ -3,14 +3,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-		vector<int> ways;
-		ways.resize(n+1, -1);
-    	ways[0] = 1;
-		ways[1] = 1;
-		for (int i = 2; i <= n; i++) {
-			ways[i] = ways[i-1] + ways[i-2];
+		int a = 1;
+		int b = 1;
+		int c = 0;
+		if (n == 0 || n == 1) {
+			return 1;
 		}
-		return ways[n];
+		for (int i = 2; i <= n; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		return c;
 	}
 };
 
