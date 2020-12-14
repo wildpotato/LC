f076f3dff8ebc9d45a88b5df8f4438754cfc73e6
Johnny Chan <JohnnyChan@yuanta.com>
2020-12-14 15:36:17 +0800

    Initial commit

---
+++ 1431/soln.cpp
@@ -0,0 +1,19 @@
+#include "../include"
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+    }
+};
+
+void test() {
+    Solution soln;
+    soln.kidsWithCandies()
+}
+
+int main() {
+    test();
+    cout << "Successful\n";
+    return 0;
+}
