7a83df67e5aabfc92052d80327703f52e476dd01
Johnny Chan <JohnnyChan@yuanta.com>
2020-12-15 08:06:46 +0800

    Add templated isArrayEqual()

--- include.h
+++ include.h
@@ -33,3 +33,17 @@ using namespace std;
 //    std::cin.tie(nullptr);
 //    return nullptr;
 //}();
+
+/* some common utility functions */
+template <typename T>
+bool isArrayEqual(vector<T> a, vector<T> b) {
+    if (a.size() != b.size()) {
+        return false;
+    }
+    for (int i = 0; i < a.size(); ++i) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
