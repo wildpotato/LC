d1a3cbbce4a887d1a7f0f97f988a71b9ca6fff5b
Johnny Chan <chanyao@umich.edu>
2020-12-21 22:57:33 +0800

    Accepted and beat 98%

--- 108/soln.cpp
+++ 108/soln.cpp
@@ -14,6 +14,27 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-
+		return buildTree(nums, 0, nums.size());
     }
+	TreeNode* buildTree(vector<int> &nums, int lo, int hi) {
+		if (lo >= hi)
+			return nullptr;
+		int mid = (lo + hi) / 2;
+		TreeNode *root = new TreeNode(nums[mid]);
+		root->left = buildTree(nums, lo, mid);
+		root->right = buildTree(nums, mid + 1, hi);
+		return root;
+	}
 };
+
+void test() {
+	vector<int> nums {-10,-3,0,5,9};
+	Solution soln;
+	assert(isBST(soln.sortedArrayToBST(nums)));
+}
+
+int main() {
+	test();
+	cout << "Successful\n";
+	return 0;
+}
