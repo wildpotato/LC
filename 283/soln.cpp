f1c42b5f645307711fd8d84c15121898a8253ff8
Johnny Chan <chanyao@umich.edu>
2020-12-26 17:58:15 +0800

    Accepted and beat 98%

--- 283/soln.cpp
+++ 283/soln.cpp
@@ -3,10 +3,29 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
+		int lastNonZeroFoundAt = 0;
+		int n = nums.size();
+		for (int i = 0; i < n; ++i) {
+			if (nums[i] != 0) {
+				nums[lastNonZeroFoundAt++] = nums[i];
+			}
+		}
+		for (int i = lastNonZeroFoundAt; i < n; ++i) {
+			nums[i] = 0;
+		}
     }
 };
 
 void test() {
-
+	vector<int> input {0,1,0,3,12};
+	vector<int> output {1,3,12,0,0};
     Solution soln;
+	soln.moveZeroes(input);
+	printArray(input);
+	assert(isArrayEqual(input, output));
+}
+
+int main() {
+	test();
+	return 0;
+}
