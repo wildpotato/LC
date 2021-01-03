e6a63378c6e07252e401133ab1a00cf2a8fce344
Johnny Chan <chanyao@umich.edu>
2021-01-03 17:20:03 +0800

    Accepted and beat 60%

--- 5/soln.cpp
+++ 5/soln.cpp
@@ -3,8 +3,29 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-
+		if (s.length() < 2) {
+			return s;
+		}
+		int start = 0, end = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			int len1 = expandFromCenter(s, i, i);
+			int len2 = expandFromCenter(s, i, i+1);
+			int len = max(len1, len2);
+			if (len > end - start) {
+				start = i - (len - 1) / 2;
+				end = i + len / 2;
+			}
+		}
+		return s.substr(start, end - start + 1);
     }
+
+	int expandFromCenter(string s, int left, int right) {
+		while (left >= 0 && right < s.length() && s[left] == s[right]) {
+			left--;
+			right++;
+		}
+		return right - left - 1;
+	}
 };
 
 void test() {
@@ -13,7 +34,8 @@ void test() {
 	assert(ans1 == "bab" || ans1 == "aba");
 	assert(soln.longestPalindrome("cbbd") == "bb");
 	assert(soln.longestPalindrome("a") == "a");
-	assert(soln.longestPalindrome("ac") == "a");
+	string ans2 = soln.longestPalindrome("ac");
+	assert(ans2 == "a" || ans2 == "c");
 }
 
 int main() {
