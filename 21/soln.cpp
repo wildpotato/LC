69c215ce7ef2ad81c946592abb608a14ad53b454
Johnny Chan <JohnnyChan@yuanta.com>
2021-01-06 10:56:56 +0800

    Accepted and beat 99%

--- 21/soln.cpp
+++ 21/soln.cpp
@@ -15,21 +15,27 @@ public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if (l1 == nullptr) return l2;
         if (l2 == nullptr) return l1;
-        ListNode *ret = nullptr;
+        ListNode *ret = new ListNode(INT_MIN); // dummy head
+        ListNode *curr = ret;
         while (l1 || l2) {
-            if (l1 == nullptr) ret->next = l2;
-            else if (l2 == nullptr) ret->next = l1;
-            else {
+            if (l1 == nullptr) {
+                curr->next = l2;
+                break;
+            } else if (l2 == nullptr) {
+                curr->next = l1;
+                break;
+            } else {
                 if (l1->val < l2->val) {
-                    ret->next = new ListNode(l1->val);
+                    curr->next = new ListNode(l1->val);
                     l1 = l1->next;
                 } else {
-                    ret->next = new ListNode(l2->val);
+                    curr->next = new ListNode(l2->val);
                     l2 = l2->next;
                 }
+                curr = curr->next;
             }
         }
-        return ret;
+        return ret->next;
     }
 };
 
@@ -39,6 +45,7 @@ void test1() {
     ListNode *ret = nullptr;
     Solution soln;
     assert(isListNodeEqual(soln.mergeTwoLists(l1, l2), ret));
+    //delete l1, l2, ret;
 }
 
 void test2() {
@@ -47,6 +54,7 @@ void test2() {
     ListNode *ret = new ListNode(0);
     Solution soln;
     assert(isListNodeEqual(soln.mergeTwoLists(l1, l2), ret));
+    //delete l1, l2, ret;
 }
 
 void test3() {
@@ -55,16 +63,14 @@ void test3() {
     ListNode *ret = new ListNode(1, new ListNode(1, new ListNode(2,
                    new ListNode(3, new ListNode(4, new ListNode(4))))));
     Solution soln;
-    cout << "OK\n";
-    assert(isListNodeEqual(soln.mergeTwoLists(l1, l2), ret));
-    cout << "OK\n";
+    ListNode *merged = soln.mergeTwoLists(l1, l2);
+    //printListNode(merged, ret);
+    assert(isListNodeEqual(merged, ret));
 }
 
 int main() {
     test1();
-    cout << "1\n";
     test2();
-    cout << "2\n";
     test3();
     cout << "Successful\n";
     return 0;
