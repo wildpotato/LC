9baaef744c115e76a34081c1f467a49644b2fe03
Johnny Chan <JohnnyChan@yuanta.com>
2021-01-21 08:07:25 +0800

    Add comment

--- 14/soln.cpp
+++ 14/soln.cpp
@@ -6,6 +6,10 @@ public:
         if (strs.size() == 0) return "";
         string prefix = strs[0];
         for (int i = 1; i < strs.size(); ++i) {
+            // the strs.find() != 0 (ZERO) is very important here
+            // if we use string::npos instead, it is not guaranteed
+            // the string contains the PREFIX since it may start anywhere
+            // in the string, so pay attention to that!
             while (size_t pos = strs[i].find(prefix, 0) != 0) {
                 prefix.erase(prefix.begin() + prefix.length() - 1);
             }
