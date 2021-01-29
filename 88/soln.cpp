af2a3e8b3db5ac83f938ab6d7505c74a4de6d148
Johnny Chan <JohnnyChan@yuanta.com>
2021-01-29 14:53:39 +0800

    Accepted and beat 100%

--- 88/soln.cpp
+++ 88/soln.cpp
@@ -1,17 +1,32 @@
 #include "../include.h"
 
+/*
+ *  Use three pointers and work from the end of first array.
+ *
+ *  - ptr1: the position in array 1, starting from m - 1
+ *  - ptr2: the position in array 2, starting from n - 1
+ *  - ptr_insert: current position to insert element into, starting
+ *    from m + n - 1
+ *
+ *  The logic follows:
+ *  Starting at ptr_insert, we keep looping until it reaches the begging
+ *  of first array. During each iteration, if ptr2 goes out of bound then
+ *  we're done (since the rest of array 1 is sorted already), we compare
+ *  the elements that ptr1 and ptr2 point to and insert the greater one
+ *  into the back of the first array.
+ *
+ */
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int ptr1 = m > 0 ? m-1 : 0;
+        int ptr1 = m-1;
         int ptr2 = n-1;
-        int ptr_insert = m+n-1;
-        while (ptr1 >= 0 and ptr2 >= 0) {
-            if (nums1[ptr1] > nums2[ptr2]) {
-                nums1[ptr_insert--] = nums1[ptr1];
-                nums1[ptr1--] = nums2[ptr2--];
+        for (int ptr_insert = m+n-1; ptr_insert >= 0; --ptr_insert) {
+            if (ptr2 < 0) break;
+            if (ptr1 >= 0 && nums1[ptr1] > nums2[ptr2]) {
+                nums1[ptr_insert] = nums1[ptr1--];
             } else {
-                nums1[ptr_insert--] = nums2[ptr2--];
+                nums1[ptr_insert] = nums2[ptr2--];
             }
         }
     }
