dc94a03bac7e293fc87cd513580aab0d09172391
Johnny Chan <JohnnyChan@yuanta.com>
2021-02-01 08:09:12 +0800

    Accepted and beat 100%

--- 226/soln.cpp
+++ 226/soln.cpp
@@ -14,6 +14,25 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-
+        if (root == nullptr) return root;
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
     }
 };
+
+void test() {
+    TreeNode *ori = new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+    TreeNode *inv = new TreeNode(4, new TreeNode(7, new TreeNode(9), new TreeNode(6)), new TreeNode(2, new TreeNode(3), new TreeNode(1)));
+    Solution soln;
+    soln.invertTree(ori);
+    assert(isTreeNodeEqual(ori, inv));
+}
+
+int main() {
+    test();
+    cout << "Successful\n";
+    return 0;
+}
