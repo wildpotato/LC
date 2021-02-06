e0be096b429c12d07b93ab187285033ab40ca170
Johnny Chan <chanyaoc@gmail.com>
2021-02-06 15:30:12 +0800

    Accepted and beat 97%

--- 204/soln.cpp
+++ 204/soln.cpp
@@ -1,25 +1,31 @@
 #include "../include.h"
 
+// The sqrt() function is simply too expensive and may cause
+// TLE (time limit exceeded) on Leetcode server, therefore we
+// must use a little trick to achieve the same logic.
+// The Sieve of Eratosthenes uses an extra O(n) memory and its runtime complexity is O(n log log n).
 class Solution {
 public:
     int countPrimes(int n) {
+        vector<bool> is_prime(n, true);
+        for (int i = 0; i < 2 && i < n; ++i) {
+            is_prime[i] = false;
+        }
+        for (int i = 2; i * i < n; i++) {
+            if (!is_prime[i]) {
+                continue;
+            }
+            for (int j = i * i; j < n; j += i) {
+                is_prime[j] = false;
+            }
+        }
         int count = 0;
-        for (int i = 2; i < n; ++i) {
-            if (isPrime(i)) {
+        for (int i = 2; i < n; i++) {
+            if (is_prime[i]) {
                 count++;
             }
         }
         return count;
-    }
-
-    bool isPrime(int num) {
-        int sq_rt = floor(sqrt(num));
-        for (int i = 2; i <= sq_rt; ++i) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 };
 
