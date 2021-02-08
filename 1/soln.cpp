6f921d6406f18f9c6f515c6bda918b4e723506f9
Johnny Chan <chanyaoc@gmail.com>
2021-02-08 21:45:44 +0800

    Accepted and beat 78%

--- 1/soln.cpp
+++ 1/soln.cpp
@@ -1,5 +1,10 @@
 #include "../include.h"
 
+// uncomment only one of the methods to run
+// #define BRUTE_FORCE
+#define HASH_TABLE
+
+#ifdef BRUTE_FORCE
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -16,10 +21,31 @@ public:
 		return ret;
     }
 };
+#endif
+
+#ifdef HASH_TABLE
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> nums_seen;
+        vector<int> ret (2, 0);
+        for (int i = 0; i < nums.size(); ++i) {
+            auto itr = nums_seen.find(target - nums[i]);
+            if (itr != nums_seen.end()) {
+                ret[0] = itr->second;
+                ret[1] = i;
+            } else {
+                nums_seen[nums[i]] = i;
+            }
+        }
+        return ret;
+    }
+};
+#endif
 
 int main() {
 	Solution soln;
 	vector<int> v1{1,2,3}, ret1{0, 2};
-	assert(soln.twoSum(v1, 4) == ret1);
+	assert(is_permutation(ret1.begin(), ret1.end(), soln.twoSum(v1, 4).begin()));
 	cout << "Successful" << endl;
 }
